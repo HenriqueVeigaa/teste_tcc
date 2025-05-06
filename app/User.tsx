ab03b53c836c0e794a4c12cfa96f306360426f73
Henrique <henrique.messias@etec.sp.gov.br>
2025-05-06 17:40:12 -0300

    Página de configurações de usuario

---
+++ app/User.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+
+
+export default function User () {
+    return(
+<View style={styles.container}>
+
+</View>
+
+
+    );
+}
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
